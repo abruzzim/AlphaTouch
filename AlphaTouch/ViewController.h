477c49237db27f545358e53c847e45f212578894
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-12-29 19:32:58 -0500

    Initial Commit

---
+++ AlphaTouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlphaTouch
+//
+//  Created by abruzzim on 12/29/14.
+//  Copyright (c) 2014 Mario Abruzzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
